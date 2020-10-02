4c1e13c7b9f9761d2d1d80bc2e1da79821f9994f
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-02 12:30:28 -0700

    set up router files

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express =  require("express")
+//import the routers 
+const actionRouter = require("./routers/actionRouter")
+const projectRouter = require("./routers/projectRouter")
+const server = express();
+server.use(express.json());
+
+//endpoints
+//request begins with /api/action use the router 
+server.use("/api/action", actionRouter);
+//request begins with /api/project use the router 
+server.use("/api/project", projectRouter) 
+
+server.get("/", (req, res) =>{
+    res.send(`<h3> API Sprint </h3>`)
+    res.status(200).json({ api: "testing for sprint", query: req.query });
+});
+
+const port = 5000;
+server.listen(port, () => {
+    console.log("\n*** Server Running on http://localhost:5000 ***\n")
+})
---
+++ routers/actionRouter.js
@@ -0,0 +1,5 @@
+const express = require("express")
+
+const Actiondb = require("../data/helpers/actionModel")
+const actionRoute = express.Router() ; 
+
